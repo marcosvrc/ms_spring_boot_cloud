1ad5667453f71cd88018588b50b8d3c9346f5258
Marcos Camara <marcosramosvc@gmail.com>
2021-06-08 21:22:47 -0300

    Project hr-api-gateway created

---
+++ hr-api-gateay/src/main/java/br/com/hrapigateway/HrApiGateayApplication.java
@@ -0,0 +1,13 @@
+package br.com.hrapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGateayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGateayApplication.class, args);
+	}
+
+}
