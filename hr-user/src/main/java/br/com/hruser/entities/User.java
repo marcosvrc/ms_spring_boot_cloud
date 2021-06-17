4e80723e591bb0950bc64ee34f1b138a2ac6c6a0
Marcos Camara <marcosramosvc@gmail.com>
2021-06-17 20:45:03 -0300

    User, Role , many to many association

---
+++ hr-user/src/main/java/br/com/hruser/entities/User.java
@@ -0,0 +1,56 @@
+package br.com.hruser.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_user")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 367875080354389873L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
+	private Long id;
+	
+	@Getter
+	@Setter
+	private String name;
+	
+	@Getter
+	@Setter
+	private String email;
+	
+	@Getter
+	@Setter
+	private String password;
+	
+	@Getter
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name  = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+}
